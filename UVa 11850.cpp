4d29ca09b46c8942a6982de267f165d9ee5b990f
Md.Samiul Alim <53310538+Samiul-25@users.noreply.github.com>
2023-04-11 21:48:30 +0000

    Add files via upload

---
+++ UVa 11850.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    int i,j,n;
+    int a[1430];
+    bool f;
+    while (cin >> n && n!=0) {
+        for (i=1;i<=n;i++)
+            cin >> a[i];
+        for (i=1;i<n;i++)
+            for (j=i+1;j<=n;j++)
+                if (a[i]>a[j]) {
+                    int temp=a[i]; a[i]=a[j]; a[j]=temp;
+                }
+        f = true;
+        if (1422-a[n]>100) f = false;
+        else
+            for (i=2;i<=n;i++)
+                if (a[i]-a[i-1]>200) {
+                    f = false; break;
+                }
+        if (f) cout << "POSSIBLE";
+        else cout << "IMPOSSIBLE";
+        cout << endl;
+    }
+}
